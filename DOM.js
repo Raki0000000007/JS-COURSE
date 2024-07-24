abaea96fcc386f67466496b0c2864c41dad8a4fb
Rakesh C <106958094+Raki0000000007@users.noreply.github.com>
2024-07-24 11:36:38 +0530

    19---DOM.js

--- DOM.js
+++
@@ -1,9 +0,0 @@
-//DOCUMENT OBJECT MODEL
-//1.innerHTML: returns all html nodes 
-//2.textContent : returns all text content
-//3.innerText : gives the text in the selected node
-
-
-
-//adding elements to the html 
-//suppose if we want to add the child li element to the existing parent li then create a function mentioning which element need to add then append the element tot he parent or child as per our requiremtn then call the functon
