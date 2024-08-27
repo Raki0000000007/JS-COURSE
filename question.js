3d86d8a51b5ef5474c353305f385921407e3a234
Rakesh C <106958094+Raki0000000007@users.noreply.github.com>
2024-08-27 21:44:59 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,26 @@
+// const Array=['Orange','Mango','Pine','Cherry','Apple']
+
+// const ol=document.createElement('ol').type='a';
+// const li_1=document.createElement('li');
+// const li_2=document.createElement('li');
+// const li_3=document.createElement('li');
+// const li_4=document.createElement('li');
+// const li_5=document.createElement('li');
+// const li_6=document.createElement('li');
+
+
+// function add_ol(){
+//        ol.innerText=`This is Ol`;
+//        document.querySelector('body').append(ol)
+// }
+// add_ol()
+
+// function add_li(){
+//     li_1.innerText=`${Array[0]}`;
+//     document.querySelector('body').append(li_1)
+// }
+// add_li()
+
+let a;
+a=5;
+console.log(a);
